221caca1aa52627fe735b026349f8517bd6bef2a
Joshua Oghogho Agbonghae <joshua.tom1@yahoo.com>
2025-07-02 18:23:17 +0100

    Add 3D cone plot R script

---
+++ 3d_plot of a Cone with RStudio.R
@@ -0,0 +1,33 @@
+
+#To illustrate simple right circular cone
+cone <- function(x, y){
+  sqrt(x ^ 2 + y ^ 2)}
+
+# prepare variables.
+x <- y <- seq(-1, 1, length = 30)
+z <- outer(x, y, cone)
+#outer() is used to apply a function to two arrays
+# plot the 3D surface
+persp(x, y, z)
+#===========================
+  # Adding Titles and Labeling Axes to Plot
+  cone <- function(x, y){
+    sqrt(x ^ 2 + y ^ 2)
+  }
+
+# prepare variables.
+x <- y <- seq(-1, 1, length = 30)
+z <- outer(x, y, cone)
+
+# plot the 3D surface
+# Adding Titles and Labeling Axes to Plot
+persp(x, y, z,
+      main="Perspective Plot of a Cone",
+      zlab = "Height",
+      theta = 60, phi = 20,#theta, phi :angles defining the viewing direction. 
+                           #theta gives the azimuthal direction:azimuth angle is 
+                     #the compass direction from which the sunlight is coming and phi the
+              #  colatitude(a colatitude is the complementary angle of a given latitude, i.e. 
+             #the difference between 90° and the latitude.)
+      col = "orange",shade=0.5)
+
